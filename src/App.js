b55966fb029d6dbd9481ce7c19f7c82488d1f3df
MatheusGorga <matheusgobar@gmail.com>
2022-12-20 01:30:52 -0300

    alura+ desafio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [endereco, setEndereco] = useState({})
+
+  function manipularEndereco(e) {
+    const cep = e.target.value
+
+    setEndereco({
+      cep: cep
+    })
+
+    if (cep && cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(res => res.json())
+        .then(data => setEndereco({
+          cep: data.cep,
+          rua: data.logradouro,
+          bairro: data.bairro,
+          cidade: data.localidade,
+          estado: data.uf
+
+        }))
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <input type='text' onChange={manipularEndereco} />
+
+        <ul>
+          <li>Cep: {endereco.cep}</li>
+          <li>Bairro: {endereco.bairro}</li>
+          <li>Rua: {endereco.rua}</li>
+          <li>Cidade: {endereco.cidade}</li>
+          <li>Estado: {endereco.estado}</li>
+        </ul>
+
+        <button type='submit' onClick={manipularEndereco}>Pesquisar</button>
+
+
       </header>
     </div>
   );
